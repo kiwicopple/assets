8b401d203272a8c5f50801d14b4eca5e63bc0106
Paul Copplestone <pcopplestone@gmail.com>
2019-12-16 20:26:42 +0800

    Fixing build

--- build.js
+++ build.js
@@ -5,28 +5,30 @@ const glob = require("glob");
 /**
  * Images
  */
-var allImages = glob.sync("src/img/**/*", { nodir: true, absolute: false });
+const inputDir = "assets/img/"
+const outputDir = "public/img/"
+var allImages = glob.sync(`${inputDir}**/*`, { nodir: true, absolute: false });
 const widths = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 const buildImages = () => {
   widths.map(width => {
-      allImages.map(async image => {
-        try {
-          const imgFullName = path.basename(image);
-          const imgExt = image.substr(image.lastIndexOf("."));
-          const imgName = imgFullName.replace(imgExt, "");
-          const outputName = imgName + "_" + width + "w" + imgExt;
+    allImages.map(async image => {
+      try {
+        const imgFullName = path.basename(image);
+        const imgExt = image.substr(image.lastIndexOf("."));
+        const imgName = imgFullName.replace(imgExt, "");
+        const outputName = imgName + "_" + width + "w" + imgExt;
 
-          const destination = image
-            .replace("src/img/", "public/img/")
-            .replace(imgFullName, outputName);
+        const destination = image
+          .replace(inputDir, outputDir)
+          .replace(imgFullName, outputName);
 
-          await sharp(image)
-            .resize(width)
-            .toFile(destination);
-        } catch (error) {
-          console.log("error", error);
-        }
-      });
+        await sharp(image)
+          .resize(width)
+          .toFile(destination);
+      } catch (error) {
+        console.log("error", error);
+      }
+    });
   });
 };
 
