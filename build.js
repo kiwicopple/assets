05585c16a0fd07087fec4b9976f6d26c17840e27
Paul Copplestone <pcopplestone@gmail.com>
2019-12-16 20:28:23 +0800

    Adding edges

--- build.js
+++ build.js
@@ -7,7 +7,7 @@ const glob = require("glob");
  */
 const inputDir = "assets/img/"
 const outputDir = "public/img/"
-var allImages = glob.sync(`${inputDir}**/*`, { nodir: true, absolute: false });
+var allImages = glob.sync(`${inputDir}**/*.png`, { nodir: true, absolute: false });
 const widths = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 const buildImages = () => {
   widths.map(width => {
@@ -26,7 +26,7 @@ const buildImages = () => {
           .resize(width)
           .toFile(destination);
       } catch (error) {
-        console.log("error", error);
+        console.log(`${image} error`, error);
       }
     });
   });
